e6b0af8e0cb5063613590222ebd8a2dcf24c000a
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-10-02 18:59:49 +0530

    Refactor-2022 (#3)
    * [refactor] sw, manifest, install, update icons
    
    - format index.html
    
    * [refactor] Buttons styling, add border-radius
    
    * [refactor] remove extra dependencies, and code
    
    - update div and mul button
    - update readme
    
    * [refactor] add lighthouse improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,37 @@
-const { src, dest, watch } = require('gulp')
+const { src, dest, watch } = require('gulp');
 const scss = require('gulp-sass');
 const browserSync = require('browser-sync');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
-const htmlmin = require('gulp-htmlmin');
-const pug = require('gulp-pug');
 
 const s = 'src/';
 const d = 'public/';
 
 function htmlTask() {
-    return src(s + 'html/*.html')
-        // .pipe(htmlmin({
-        //     collapseWhitespace: true,
-        //     removeComments: true
-        // }))
-        .pipe(dest(d))
-    // .pipe(browserSync.stream());
-}
-
-function pugTask() {
-    return src(s + 'pug/*.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .pipe(dest(d))
-        .pipe(browserSync.stream());
+    return src(s + 'html/*.html').pipe(dest(d));
 }
 
 function scssTask() {
     return src(s + 'styles/scss/*.scss')
-        .pipe(scss({
-            outputStyle: 'compressed'
-        }).on('error', scss.logError))
+        .pipe(
+            scss({
+                outputStyle: 'compressed',
+            }).on('error', scss.logError)
+        )
         .pipe(dest(d + 'styles/'))
         .pipe(browserSync.stream());
 }
 
 function scriptTask() {
-    return src(s + 'scripts/*.js')
-        .pipe(dest(d + 'scripts/'))
+    return src(s + 'scripts/*.js').pipe(dest(d + 'scripts/'));
 }
 
 function manifest() {
-    return src(s + 'others/manifest.json')
-        .pipe(dest(d))
+    return src(s + 'others/manifest.json').pipe(dest(d));
 }
 
 function serviceWorker() {
-    return src(s + 'others/sw.js')
-        .pipe(dest(d))
+    return src(s + 'others/sw.js').pipe(dest(d));
 }
 
 function imageminTask() {
@@ -62,17 +44,16 @@ function imageminTask() {
 
 function serverTask() {
     browserSync.init({
-        server: d
-    })
+        server: d,
+    });
 
-    watch(s + 'html/*.html', { ignoreInitial: false }, htmlTask)
-    // watch(s + 'pug/*.pug', { ignoreInitial: false }, pugTask)
-    watch(s + 'styles/scss/**/*.scss', { ignoreInitial: false }, scssTask)
-    watch(s + 'scripts/**/*.js', { ignoreInitial: false }, scriptTask)
-    watch(s + 'images/**/*', { ignoreInitial: false }, imageminTask)
-    watch(s + 'others/manifest.json', { ignoreInitial: false }, manifest)
-    watch(s + 'others/sw.js', { ignoreInitial: false }, serviceWorker)
+    watch(s + 'html/*.html', { ignoreInitial: false }, htmlTask);
+    watch(s + 'styles/scss/**/*.scss', { ignoreInitial: false }, scssTask);
+    watch(s + 'scripts/**/*.js', { ignoreInitial: false }, scriptTask);
+    watch(s + 'images/**/*', { ignoreInitial: false }, imageminTask);
+    watch(s + 'others/manifest.json', { ignoreInitial: false }, manifest);
+    watch(s + 'others/sw.js', { ignoreInitial: false }, serviceWorker);
     watch(d + '**/*').on('change', browserSync.reload);
 }
 
-exports.default = serverTask
+exports.default = serverTask;
--- src/scripts/install.js
+++ src/scripts/install.js
@@ -1,45 +1,48 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('../sw.js')
-            .then(function (registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-            }, function (err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
-            });
-    });
-} else {
-    console.log('The browser does not support the service worker !!!');
-}
+let deferredPrompt;
+const btnAdd = document.querySelector('.checkBtn');
+
+const registerServiceWorker = async () => {
+    if ('serviceWorker' in navigator) {
+        try {
+            const reg = await navigator.serviceWorker.register('../sw.js');
+
+            console.log('[Install] SW reg. successful, Scope: ', reg.scope);
+        } catch (error) {
+            console.log('ServiceWorker registration failed: ', error);
+        }
+    } else {
+        console.log('[Install] Browser does not support SW');
+    }
+};
+
+window.addEventListener('load', () => {
+    registerServiceWorker();
+});
 
 // For prompting the install banner
-let deferredPrompt;
-let btnAdd = document.querySelector('.checkBtn');
-
 window.addEventListener('beforeinstallprompt', (e) => {
     // Prevent Chrome 67 and earlier from automatically showing the prompt
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
 
-    console.log('btn Displayed')
+    console.log('[Install] Btn displayed');
     btnAdd.style.display = 'block';
 });
 
 btnAdd.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
     btnAdd.style.display = 'none';
+    console.log('[Install] Btn hide');
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice
-        .then((choiceResult) => {
-            if (choiceResult.outcome === 'accepted') {
-                console.log('User accepted the A2HS prompt');
-            } else {
-                console.log('User dismissed the A2HS prompt');
-            }
-            deferredPrompt = null;
-        });
+    deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+            console.log('[Install] User accepted the A2HS prompt');
+        } else {
+            console.log('[Install] User dismissed the A2HS prompt');
+        }
+        deferredPrompt = null;
+    });
 });
