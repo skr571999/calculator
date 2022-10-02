9eca9a9f853769e4e6354320e3d3b72010f8f42b
Sachin Kumar Rajput <31756244+skrmain@users.noreply.github.com>
2022-10-02 11:46:21 +0000

    [refactor] sw, manifest, install, update icons
    - format index.html

--- src/others/sw.js
+++ src/others/sw.js
@@ -1,72 +1,79 @@
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
+const cacheName = 'v1';
+const contentToCache = [
+    './index.html',
     './styles/main.css',
-    './images/icon.png',
     './images/clear.png',
-    './images/icon-192x192.png',
-    './images/icon-512x512.png',
+    './images/icon-192.png',
+    './images/icon-512.png',
     './scripts/main.js',
-    './scripts/install.js'
+    './scripts/install.js',
 ];
 
-self.addEventListener('install', function (event) {
-    // Perform install steps
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
+self.addEventListener('install', (e) => {
+    console.log('[sw] Install');
+    e.waitUntil(
+        (async () => {
+            const cache = await caches.open(cacheName);
+            console.log('[sw] caching all');
+            await cache.addAll(contentToCache);
+        })()
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
+self.addEventListener('fetch', (e) => {
+    console.log('[sw] Fetch');
+    e.respondWith(
+        (async () => {
+            // console.log('[sw] Fetching : ', e.request.url);
+            // console.log('[sw] Request', e.request);
+            const r = await caches.match(e.request);
+            // console.log('[sw] Cache Response', r);
+            if (r) return r;
 
-                return fetch(event.request).then(
-                    function (response) {
-                        // Check if we received a valid response
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
+            // console.log('[sw] Not Found in Cache');
+            const response = await fetch(e.request);
+            // console.log('[sw] Response ', response);
+            // Check if we received a valid response
+            if (
+                !response ||
+                response.status !== 200 ||
+                response.type !== 'basic' ||
+                e.request.method !== 'GET'
+                // To cache only 'contentToCache'
+                // !contentToCache.find((val) => response.url.includes(val.substring(1)))
+            ) {
+                return response;
+            }
 
-                        // IMPORTANT: Clone the response. A response is a stream
-                        // and because we want the browser to consume the response
-                        // as well as the cache consuming the response, we need
-                        // to clone it so we have two streams.
-                        var responseToCache = response.clone();
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
 
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
+            const cache = await caches.open(cacheName);
+            cache.put(e.request, response.clone());
+            return response;
+        })()
     );
 });
 
-self.addEventListener('activate', function (event) {
-
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+self.addEventListener('activate', (event) => {
+    console.log('[sw] Activate');
+    const cacheWhitelist = [];
 
     event.waitUntil(
-        caches.keys().then(function (cacheNames) {
+        (async () => {
+            const cacheNames = await caches.keys();
+            // console.log('[sw] Keys ', cacheNames);
             return Promise.all(
-                cacheNames.map(function (cacheName) {
+                cacheNames.map((cacheName) => {
+                    // console.log('[sw] Cache ', cacheName);
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        console.log('[sw] Deleting cache - ', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
             );
-        })
+        })()
     );
 });
